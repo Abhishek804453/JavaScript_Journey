c83d7d03679f48faadabbead569cf74016144586
ABHISHEK KUMAR <123956583+Abhishek804453@users.noreply.github.com>
2026-02-20 14:26:52 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+// alert("Hello world") // we are using node.js , not browser
+
+// Avoid writing multiple statements on one line (bad readability)
+// console.log("Hello world"); console.log("Abhi")
+
+console.log("Hello world")
+console.log("Abhi")
+
+let name = "Abhishek"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof age);  // number
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
