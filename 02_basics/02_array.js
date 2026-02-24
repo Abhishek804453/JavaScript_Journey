c013dae67e4028dd89fa5f203d3ddc096c85f5a6
ABHISHEK KUMAR <123956583+Abhishek804453@users.noreply.github.com>
2026-02-24 18:42:07 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -18,4 +18,18 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 
 const another_array = [1,2,3, [4,5,6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat();
+const real_another_array = another_array.flat(Infinity);
+
+console.log(real_another_array);
+
+console.log(Array.isArray("Abhishek"))
+
+console.log(Array.from("Abhishek"));
+
+console.log(Array.from({name : "Abhishek"})) // interesting point
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
